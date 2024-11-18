3facd2980e419abfe9cd80ed53d53b744aeb2eed
Sherwin <87555304+owenlim225@users.noreply.github.com>
2024-11-18 23:22:22 +0800

    splash screen goods

--- splash_screen.gd
+++ splash_screen.gd
@@ -1,8 +1,11 @@
 extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-const TITLE_SCREEN = preload("res://scenes/title_screen.tscn")
+var Main = preload("res://scenes/title_screen.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation_player.play()
-	get_tree().change_scene(TITLE_SCREEN)
+
+	
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_packed(Main)

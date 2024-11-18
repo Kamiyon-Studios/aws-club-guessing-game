1ab76720ce36b1df916d8ee23535ea3c34d31f1f
Sherwin <87555304+owenlim225@users.noreply.github.com>
2024-11-18 22:48:56 +0800

    title screen animation

--- title_screen.gd
+++ title_screen.gd
@@ -1,9 +1,12 @@
 extends Control
+@onready var music_title_screen: AudioStreamPlayer = $music_title_screen
+@onready var anim_aws_logo: AnimationPlayer = $anim_aws_logo
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AudioStreamPlayer.play()
+	music_title_screen.play()
+	anim_aws_logo.play()
 
 
 func _on_btn_new_game_pressed() -> void:
